70c8060c73ee0ee39fbc50d60048a79942d5065e
aliasghar112 <asgharcallof1386@gmail.com>
2021-04-10 12:38:23 +0430

    hi ali

---
+++ C++/project1.cpp
@@ -0,0 +1,20 @@
+ // A hello world program in C++
+
+	#include<iostream>
+
+	using namespace std;
+
+int main(){
+
+
+		enum Day {Sat , Sun , Mon , Tues , Wendnes , Thurs ,Fri};
+
+
+
+		cout << Sat + Sun <<endl ;
+		cout << Sat <<endl ;
+		cout << Mon - Tues<<endl ;
+		cout <<Wendnes / Fri <<endl ;
+
+		return 0 ;
+	}
